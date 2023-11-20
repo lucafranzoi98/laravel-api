ee8ea8dc5fa9eebcb81ce901be29972e4b1ec3bb
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-20 16:42:33 +0100

    add cors controll and show method

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,10 +8,27 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index (){
+    public function index()
+    {
         return response()->json([
             'status' => 'Success',
             'result' => Project::with('type', 'technologies')->orderByDesc('id')->paginate(9)
+        ]);
+    }
+
+    public function latest()
+    {
+        return response()->json([
+            'status' => 'Success',
+            'result' => Project::with('type', 'technologies')->orderByDesc('id')->take(3)->get()
+        ]);
+    }
+
+    public function show($slug)
+    {
+        return response()->json([
+            'status' => 'Success',
+            'result' => Project::where('slug', $slug)->first()
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects-latest', [ProjectController::class, 'latest']);
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
